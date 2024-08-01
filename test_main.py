d5c96a345d3723387161ded52c4225b441c9e6f4
Mariano Bello <marianobf23@gmail.com>
2024-08-01 09:50:53 -0400

    removed post stage for now

--- test_main.py
+++ test_main.py
@@ -1,17 +1,15 @@
-# test_main.py
 import unittest
-import xmlrunner
-from main import add, subtract
+from main import add_int, sub_int
 
 class TestMain(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-1, 1), 0)
+    def test_add_int(self):
+        self.assertEqual(add_int(1,1), 2)
+        self.assertEqual(add_int(4,5), 9)
 
-    def test_subtract(self):
-        self.assertEqual(subtract(2, 1), 1)
-        self.assertEqual(subtract(2, 2), 0)
+    def test_sub_int(self):
+        self.assertEqual(sub_int(10,5), 5)
+        self.assertAlmostEqual(sub_int(4,6), -2) # Negative numbers accepted
 
-if __name__ == "__main__":
-    with open('test-results.xml', 'wb') as output:
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output))
+
+if __name__ == '__main__':
+    unittest.main()
